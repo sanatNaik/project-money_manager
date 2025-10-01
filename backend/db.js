731c4b99622b8db4b62af369daeb2dcfdfe16765
sanatNaik <141733066+sanatNaik@users.noreply.github.com>
2025-10-01 10:00:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'sanatSQL@2025',
+    password: '##',
     database: 'money_manager',
     port:3306
 });
